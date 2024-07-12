a8f53680437d066eee81df41df937919443ff206
[Mariam-Aamir] <[ummeareesh691@gmail.com]>
2024-07-12 16:45:48 +0500

    31th  Assignment

---
+++ main.ts
@@ -0,0 +1,23 @@
+//Asignment 31
+
+// No User : Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+
+// • Remove all of the usernames from your array, and make sure the correct message is printed
+
+
+let usersName = ['Rayan', 'Manzoor', 'Admin', 'Hamza', 'Umer'];
+
+usersName = []
+
+if(usersName.length === 0){
+     console.log(' your Array is empty We need to find some users!');
+}else{
+usersName.forEach(oneUser => {
+   if (oneUser === "Admin") {
+    console.log(`Hello ${oneUser},would you like to see a status report?`);
+    } else {
+        console.log(`Hello ${oneUser}, thank you for logging in again.`);
+    }
+});
+}
